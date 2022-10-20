022926b814053e49e4f4d60dd537331164de3a3f
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-19 20:00:58 -0400

    All files working

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,33 @@
+class PlantsController < ApplicationController
+  wrap_parameters format: []
+
+  # GET /plants
+  def index
+    render json: Plant.all
+  end
+
+  # GET plants/:id
+  def show
+    plant = Plant.find(params[:id])
+    if plant
+      render json: plant
+    else
+      render json: {
+        error: "Plant not found"
+      }, status: :not_found
+    end
+  end
+
+  # POST /plants
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, status: :created
+  end
+
+  private 
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+
+end
